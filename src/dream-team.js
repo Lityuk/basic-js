684b3affb3db68177381ebbe86e4a4c9beb4146b
Lityuk <lityuk@gmail.com>
2021-03-25 16:49:09 +0300

    feat: solve dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  let arr = members.map(function (element) {
+    if (typeof element != "string") {
+      return;
+    } else if (element[0] == " ") {
+      element = element.trim();
+    } else console.log(element);
+
+    return element[0];
+  });
+
+  return arr.join("").toUpperCase().split("").sort().join("") || false;
 };
